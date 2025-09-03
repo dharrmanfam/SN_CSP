d6067a829b8b778e53dadb298a97ab12d7ce998e
sushidontlie520 <sophie.nam@ucas-edu.net>
2025-09-03 19:29:22 +0000

    fixed coding for hello.World

--- Variables/hello.World.py
+++ Variables/hello.World.py
@@ -3,5 +3,5 @@ name = input("what is your name? ")
 print("Hello, " + name + "! Nice to meet you.")
 
 dog = input("which dog breed do you like the most? ")
-print(dog + "s are a beautiful breed! ")
+print(dog + "'s are a beautiful breed! ")
 
