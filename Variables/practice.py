421ffda3f2b9bed1c485fb9ffc1673c840a85f45
sushidontlie520 <sophie.nam@ucas-edu.net>
2025-09-03 03:29:39 +0000

    practice p. file

--- Variables/practice.py
+++ Variables/practice.py
@@ -1,11 +1,12 @@
+#SN 6th
 name = "Sophie"
-smlNum = 2
-lrgNum = 670
+smlNum = "2"
+lrgNum = "670"
 breakfast = "waffles"
 favorite_color = "blue"
 school_name = "UCAS"
-year = 2025
+year = "2025"
 eye_color = "brown"
-age = 15
+age = "15"
 favorite_subject = "PE"
 print(name, smlNum, lrgNum, breakfast, favorite_color, school_name, year, eye_color, age, favorite_subject)
