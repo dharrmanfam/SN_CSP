6f54be09e7fa5995a54f4c67bdeecb613815bde4
dharrmanfam <sophie.nam@ucas-edu.net>
2025-09-04 13:43:28 -0600

    notes.py

---
+++ Variables/notes.py
@@ -0,0 +1,44 @@
+# SN 6th Expressions Notes
+
+#What is an algorithm?
+    # a collection of numbers and symbols in order to get a result.
+#List steps in an algorithm
+    # 1.) Variables/pieces of info needed
+    # 2.) Process info
+#List ALL of the different mathematical operators
+    # "+"= addition
+    # "-"= subtraction
+    # "*"= multiply
+    # "/"= division
+#List ALL of the assignment operators
+    # 
+name = input("what is your name? ")
+print("Hello", name)
+
+#algorithm for area
+length = 5
+width = 20
+area = length * width 
+print(length, "X", width, "=", area)
+
+#algorithm example
+age_1 = 13
+age_2 = 14
+age_3 = 14
+age_4 = 15
+total = age_1 + age_2 + age_3 + age_4
+average = (total/4)
+print("The average age between my friends are", + average)
+
+#math equations
+num_one = 10
+num_two = 6
+
+print("addition:", num_one + num_two)
+print("subtraction:", num_one - num_two)
+print("multiplication", num_one * num_two)
+print("division:", num_one / num_two)
+print("exponents:", num_one ** num_two)
+print("integer division:", num_one // num_two) 
+print("modulo (remainder):", num_one % num_two) 
+      
