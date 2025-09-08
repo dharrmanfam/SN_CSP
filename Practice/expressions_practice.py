01eff3b2296c5c32339b66f2976bee1aa2375352
sushidontlie520 <sophie.nam@ucas-edu.net>
2025-09-08 01:00:43 +0000

    first_program.py and expressions_practice.py

--- Practice/expressions_practice.py
+++ Practice/expressions_practice.py
@@ -1,3 +1,26 @@
 # SN 6th Expressions Practice
 
-print("",)
+apple = "(7-24/8*4+6)="
+orange = "(18/3-7+2*5)="
+pineapple = "6*4/12+72/8-9)="
+peach = "(17-6/2)+4*3)="
+cherry = "-2(1*4-2/2)+(6+2-3)="
+banana = "-1*[(3-4*7)5]-2*24/6)="
+lemon = "(3*5**2/15)-(5-2**2)="
+mango = "(1**4*2**2+3**3)-2**5/4="
+dragonfruit = "(22/2-2*5)**2+(4-6/6)**2)="
+print(apple, (7-24/8*4+6))
+print(orange, (18/3-7+2*5))
+print(pineapple,(6*4/12+72/8-9))
+print(peach,(17-6/2)+4*3)
+print(cherry, (-2*(1*4-2/2)+(6+2-3)))
+print(banana,(-1*((3-4*7)/5))-2*24/6)
+print(lemon,((3*5**2/15)-(5-2**2)))
+print(mango,((1**4*2**2+3**3)-2**5/4))
+print(dragonfruit,((22/2-2*5)**2+(4-6/6)**2))
+
+                            
+
+
+
+
