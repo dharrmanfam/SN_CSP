8d7c7b60ac326b2cab0c008b8071a3d771762070
dharrmanfam <sophie.nam@ucas-edu.net>
2025-09-08 13:44:47 -0600

    string_notes.py

--- Notes/string_notes.py
+++ Notes/string_notes.py
@@ -1,4 +1,25 @@
 #SN 6th Strings Notes
 
 print("I did it!")
+#What makes something a string?
+    #A string is a collection of symbols held together by quotation marks including numbers, letters, puncutation
+    #A string is the only data type that allow letters.
+first_name = input("What is your first name?"). strip().title()
 
+last_name = input("What is your last name?"). strip().title()
+
+full_name = first_name + " " + last_name
+
+sentence = "The quick brown fox jumps over the lazy dog." 
+
+print("Welcome to my program", full_name + "!")
+#Why do we have strings?
+#How do stupid proofing and sanitization relate to each other?
+#What is debugging?
+#How do you debug the different types of errors?
+#Describe what each of the methods below does:
+    #a.)find()
+    #b.)concatenate (add): 
+    #c.)upper()
+    #d.)lower()
+    #e.)strip()
