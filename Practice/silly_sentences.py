27f17acc2a5ad3cf09773033efea9104652d0492
sushidontlie520 <sophie.nam@ucas-edu.net>
2025-09-16 01:53:36 +0000

    silly_sentences.py

--- Practice/silly_sentences.py
+++ Practice/silly_sentences.py
@@ -3,4 +3,4 @@
 name = input("What is your name?"). strip() .title()
 verb = input("Give me a verb ending with -ing!"). strip() .lower()
 adverb = input("Give me an adverb!"). strip() .lower()      
-print(name, "was", adverb, verb + ".")
+print(name, "was", adverb, verb + ".") 
