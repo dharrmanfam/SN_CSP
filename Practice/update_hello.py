e87d3f9ddff0fff0e895f99d12263390fbe6919d
dharrmanfam <sophie.nam@ucas-edu.net>
2025-09-16 13:46:00 -0600

    update hellow world assignement

---
+++ Practice/update_hello.py
@@ -0,0 +1,13 @@
+#SN 6th Update Hellow World Python
+
+def welcome(name):
+    print("Hello", name.strip().title())
+    
+welcome("Sophie")
+welcome("Charlie")
+welcome("ted")
+welcome("tim")
+welcome("claire")
+welcome("emma")
+
+
