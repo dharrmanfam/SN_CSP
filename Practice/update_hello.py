86410b596ec7c0c51925017d05f4b9694101012d
sushidontlie520 <sophie.nam@ucas-edu.net>
2025-09-17 03:24:21 +0000

    update_hello.py homework updated

--- Practice/update_hello.py
+++ Practice/update_hello.py
@@ -1,13 +1,12 @@
 #SN 6th Update Hellow World Python
 
-def welcome(name):
-    print("Hello", name.strip().title())
-    
-welcome("Sophie")
-welcome("Charlie")
-welcome("ted")
-welcome("tim")
-welcome("claire")
+
+def welcome(name): 
+    print("Hello", name.strip().title() + "!") 
+
+welcome("Sophie") 
+welcome("Charlie") 
+welcome("ted") 
+welcome("tim") 
+welcome("claire") 
 welcome("emma")
-
-
