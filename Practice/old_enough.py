ec72eff5ba55c97be34f85fff1297ad53069e2ea
dharrmanfam <sophie.nam@ucas-edu.net>
2025-09-18 13:42:51 -0600

    old enough practice

--- Practice/old_enough.py
+++ Practice/old_enough.py
@@ -1,1 +1,14 @@
 #SN 6th Old Enough
+
+age = int(input("How old are you?"))
+
+if age >= 18:
+    print("You are old enough to vote!")
+elif age >= 16:
+    print("You can drive!")
+elif age >= 15:
+    print("You can get your learner's permit!")
+elif age >= 4:
+    print("Go to school!")
+else:
+    print("You are not old enough to do any of these options yet!")
