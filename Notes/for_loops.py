5d740e0359298bc165571ba06213b4ca4bd2c029
dharrmanfam <sophie.nam@ucas-edu.net>
2025-09-19 13:44:18 -0600

    time of day file and loops notes

---
+++ Notes/for_loops.py
@@ -0,0 +1,14 @@
+#SN 6th Time and For Loops Notes!
+import datetime
+
+
+current = datetime.datetime.now()
+hour = current.hour
+
+#print(f("The time in seconds since Jan 1,1960:{epoch}")
+print(f"The time is: {current}")
+print(f"The hour is: {hour}")
+
+#Lists
+siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia","Treyson","Xavier", "Jake"["Joseph", "Noah", "Zee"]]
+#Lists are in straight brackets
---
+++ Practice/time_practice.py
@@ -0,0 +1,10 @@
+#SN 6th Time of Day
+
+time = input("What is the time in the 24 hour format?")
+
+if time >0:
+    print("Good morning")
+elif time >=18:
+    print("Good Afternoon")
+else: 
+    print("")
