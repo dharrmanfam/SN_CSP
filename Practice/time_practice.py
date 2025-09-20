c4835c6e6fff6326e38197fa7dd4b7bb112588e8
sushidontlie520 <sophie.nam@ucas-edu.net>
2025-09-20 03:12:57 +0000

    time_practice.py

--- Practice/time_practice.py
+++ Practice/time_practice.py
@@ -1,10 +1,12 @@
 #SN 6th Time of Day
 
-time = input("What is the time in the 24 hour format?")
+time = int(input("What is the time now in the 24 hour format?"))
 
-if time >0:
-    print("Good morning")
-elif time >=18:
-    print("Good Afternoon")
-else: 
-    print("")
+if time >=0 and time<12:
+    print("Good Morning!")
+elif time <=17 and time>=12:
+    print("Good Afternoon!")
+elif time >17 and time<24:
+    print("Good Evening!")
+else:
+    print("Give me an actual time buddy..")
