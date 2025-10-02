30296ad1589ee79831a4ee67fd32959ebae1f0ac
dharrmanfam <sophie.nam@ucas-edu.net>
2025-10-02 13:44:01 -0600

    variables.c

---
+++ c_programming/Notes/variables.c
@@ -0,0 +1,19 @@
+// SN 6th Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    printf("%d",num);
+
+    return 0;
+}
+ // The int in front of the statement show what type of data to expect and CANNOT be changed
+ // Static: typing can't change later
+ // Dynamic: Performs manual memory management
+ // Float: Decimal number
+ // Char: Holds only one letter
+ // Bool: Checks if something is true
+ char grade = 'A'; 
+ char name[]= "Sophie";
+ int num = 4;
+ float pi = 3.14;
