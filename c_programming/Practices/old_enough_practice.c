6dc1bcac4666c53f2c23705b37b8131174f72a57
dharrmanfam <sophie.nam@ucas-edu.net>
2025-10-13 12:56:28 -0600

    old enough practice

--- c_programming/Practices/old_enough_practice.c
+++ c_programming/Practices/old_enough_practice.c
@@ -1,6 +1,6 @@
 //SN 6th Old Enough Practice
 #include<stdio.h>
-#include<stirng.h>
+#include<string.h>
 
 int main(void){
     int age;
@@ -11,13 +11,17 @@ int main(void){
         printf("You are old enough to vote!\n");
     }
     else if (age >=16){
-        printf("You are old enough to get drive!\n");
+        printf("You are old enough to drive!\n");
     }
     else if (age >= 15){
         printf("You are old enough to get a learner's permit!\n");
     }
-    else if (age < 15){}
+    else if (age < 15){
         printf("Go to school bud\n");
+    }else{
+        printf("You are not eligible for any of these options...");
+    }
+    
     
 
 
