80f1b0a3c7c943707a2301ad9e22fb891bb04991
dharrmanfam <sophie.nam@ucas-edu.net>
2025-10-14 05:18:12 +0000

    time of day practice

--- c_programming/Practices/time_of_day.c
+++ c_programming/Practices/time_of_day.c
@@ -2,3 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
+int main(void){
+    int time;
+    char name[1001];
+    printf("What is the time in military form without colons??: ");
+    scanf("%d", &time);
+
+        if (time <= 1159) {
+        printf("Good morning!\n");
+     } else if (time >= 1200) {
+        printf("Good afternoon!\n");
+    } else if (time >= 1800 ) {
+        printf("Good evening!\n");  
+    } else if (time >= 2100 ) {
+        printf("Good night!\n");
+    } else {
+        printf("Please enter a real time.\n");
+    }
+
+
+    return 0; 
+}
+
