92544ee5bfdfd6ceaca164b623d32c69259a30cc
dharrmanfam <sophie.nam@ucas-edu.net>
2025-10-15 03:54:26 +0000

    functions notes finished

--- c_programming/Notes/functions_notes.c
+++ c_programming/Notes/functions_notes.c
@@ -58,22 +58,22 @@ How to write a function in C
     Then call the function inside of main
 
 What are arguments and parameters?
-
-
+    Parameters are placeholders inside a function definition and act like variables that receive values when the function is called
+    Arguments are the actual values you pass to a function when you call it.
 
 How do arguments and parameters work together?
-
+    Parameters are set first are like a variable at first then when called by the actual value which is the argument, it gets called.
 
 How to use parameters and arguments in python
-
+    First, define a function with parameters, then call it back with arguments.
 
 What are return statements?
-
+    It is a command that ends a function and sends a value back to the part of the program that called that function.
 
 How do return statements change how you define a function in C?
-
+    When declaring the data type, you'll return the statement inside the function. When using void, it won't return anything.
 
 What do return statements do with the information
-
+    A return statement sends information from inside a function back to the place where the function was called
 
 */
