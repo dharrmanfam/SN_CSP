3eb24f359ebb0c4591fc517ff7e6af38bfb8e53a
dharrmanfam <sophie.nam@ucas-edu.net>
2025-10-21 13:15:29 -0600

    random.c

---
+++ c_programming/Notes/random.c
@@ -0,0 +1,13 @@
+//SN 6th Random Numbers
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void){
+    srand(time(NULL));
+    for (int i=0; i<10; i++){
+    int num = rand() %20 +1;
+    printf("%d\n", num);}
+    
+    return 0;
+}
--- c_programming/Practices/update_financial_calculator.c
+++ c_programming/Practices/update_financial_calculator.c
@@ -1,1 +1,4 @@
 //SN 6th Update on Financial Calculator
+#include <stdio.h>
+#include <string.h> 
+
